289cdefad74b31f79077450e403154fda62c2c8e
Pavel Berezan <pavelberezan1998@gmail.com>
2023-10-29 13:57:54 +0300

    refactoring

--- sorter.py
+++ sorter.py
@@ -2,25 +2,15 @@ from time import time
 from random import randint
 
 
-
-
 data = list(randint(-30, 30) for i in range(40))
-print(data)
+print(f"Origin list: {data}")
 
-# start_time = time()
-# for i in range(len(data)):
-#     for j in range(i+1, len(data)):
-#         if data[j] < data[i]:
-#             data[j], data[i] = data[i], data[j]
-# end_time = time() - start_time
-
-# print(data)
-# print(end_time)
-
-
+start_time = time()
 for i in range(len(data)):
-    for j in range(i+1, len(data)):
+    for j in range(i + 1, len(data)):
         if data[j] < data[i]:
             data[j], data[i] = data[i], data[j]
+end_time = time() - start_time
 
-print(data)
+print(f"Sorted list: {data}")
+print(f"Function running time: {end_time}")
